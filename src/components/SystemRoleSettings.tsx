786753e93dd1ece323576684e060492c0126bdd0
Susan <susansunnison@gmail.com>
2023-04-17 04:08:39 +0700

    Update SystemRoleSettings.tsx

--- src/components/SystemRoleSettings.tsx
+++ src/components/SystemRoleSettings.tsx
@@ -60,7 +60,7 @@ export default (props: Props) => {
             />
           </div>
           <button onClick={handleButtonClick} gen-slate-btn>
-            Set
+            Atur
           </button>
         </div>
       </Show>
